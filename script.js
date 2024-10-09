9bff3e491c6eb1b26506d97a7a6f88473cb0ef7a
fatmanurgencdogan <fgencdogan@gmail.com>
2024-10-09 13:20:32 +0300

    popüler film verileri siteye entegre edildi. Ufak düzenlemeler gerekmekte.

--- script.js
+++ script.js
@@ -1,36 +1,55 @@
-var slideIndex = 1;
+const API_KEY = 'f3090c60bbf7d2ca05c8a7afe0f60361';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&api_key=' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const main = document.getElementById('main');
 
-// Sayfa yüklendiğinde ilk slaytı göster
-document.addEventListener('DOMContentLoaded', function() {
-  showSlides(slideIndex);
-});
+getMovies(API_URL);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function getMovies(url){
+
+  fetch(url).then(res => res.json()).then(data => {
+    console.log(data.results)
+    showMovies(data.results);
+  })
+  .catch(error => console.log('Hata:', error)); // Hata mesajını kontrol et
+
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1; }
-  if (n < 1) { slideIndex = slides.length; }
+function showMovies(data){
+  main.innerHTML = '';
   
-  // Tüm slaytları gizle
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  
-  // Tüm noktaların "active" sınıfını kaldır
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  data.forEach(movie => {
+    const {title, poster_path, vote_average, overview} = movie;
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = `
+        <img src="${IMG_URL+poster_path}" alt="${title}">
 
-  // Geçerli slaytı göster
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getColor(vote_average)}">${vote_average}</span>
+      </div>
+
+      <div class="overview">
+
+        <h3>overview</h3>
+        ${overview}
+      </div>
+    `
+
+    main.appendChild(movieEl);
+  })
 }
+
+function getColor(vote){
+  if(vote >= 8){
+    return 'green'
+  }
+  else if(vote >= 5){
+    return "orange"
+  }
+  else{
+    return 'red'
+  }
+}  
